5ce4ebf8f7a556c555de19d3a1c245663b4e0f71
Daniel Sasse <dsasse07@gmail.com>
2020-11-18 20:13:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -280,12 +280,10 @@ def long_name_steals_a_ton?
   stat_by_player = collect_stat_by_name(:steals)
   player_with_max_stat = find_max_stat_by_player(stat_by_player)
   long_name = player_with_longest_name
-  binding.pry
   if player_with_max_stat == player_with_longest_name
     result = true 
   else
     result = false
   end
   result
-  binding.pry
 end 
