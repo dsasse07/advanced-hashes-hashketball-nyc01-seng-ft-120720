cca64eabd6994a03f8b3dab1f34e03fa6342664d
Daniel Sasse <dsasse07@gmail.com>
2020-11-18 19:58:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -271,9 +271,8 @@ end
 
 def player_with_longest_name
   stat_by_player = collect_stat_by_name(:player_name)
-  binding.pry
   name_length_values = stat_by_player.each_with_object ({}) { |(k,v),hash| hash[k] = v.length}
-  binding.pry
   longest_name = find_max_stat_by_player(stat_by_player)
+  binding.pry
   longest_name
 end
